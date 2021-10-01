d6b08026132cae7ea3248e115685872360e0857c
윤창현 <changh950@gmail.com>
2021-10-01 17:23:11 +0900

    Add: 초기세팅

---
+++ thisisnevercode-frontend/src/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main/Main';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import ProductList from './pages/ProductList/ProductList';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/main' component={Main} />
+          <Route exact path='/productDetail' component={ProductDetail} />
+          <Route exact path='/productList' component={ProductList} />
+          <Route exact path='/signIn' component={SignIn} />
+          <Route exact path='/signUp' component={SignUp} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ thisisnevercode-frontend/src/pages/SignIn/SignIn.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './SignIn.scss';
+
+class SignIn extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SignIn;
