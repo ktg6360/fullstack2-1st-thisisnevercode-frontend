7372483042dc1bda231f50ba519f1177d92a4081
윤창현 <changh950@gmail.com>
2021-10-06 13:38:24 +0900

    Fix: SignUp Code

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -8,12 +8,30 @@ class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-      id: '',
+      name: '',
+      email: '',
       password: '',
       isActive: false,
       seePw: false,
     };
   }
+
+  handleInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value }, () => this.handleBtn());
+  };
+
+  handleBtn = () => {
+    const { name, email, password } = this.state;
+    this.setState({
+      isActive:
+        name.includes('/^[가-힣]+$/') &&
+        email.includes(
+          '/^([w-]+(?:.[w-]+)*)@((?:[w-]+.)*w[w-]{0,66}).([a-z]{2,6}(?:.[a-z]{2})?)$/'
+        ) &&
+        password.length >= 5,
+    });
+  };
 
   changeIcon = () => {
     this.setState({
@@ -21,22 +39,49 @@ class SignUp extends Component {
     });
   };
 
+  openAlert = () => {
+    alert('유효한 형식으로 작성해주세요.');
+  };
+
+  goToList = () => {
+    if (this.state.isActive) {
+      this.props.history.push('./SignIn');
+    }
+  };
+
   render() {
-    const { seePw } = this.state;
+    const { seePw, isActive } = this.state;
     return (
       <section className='signUp'>
         <form action='' className='form'>
           <div className='signUpBox'>
             <div className='inputBox'>
               <p className='name'>이름</p>
-              <input className='nameBox' type='text' placeholder='이름' />
+              <input
+                className='nameBox'
+                type='text'
+                placeholder='이름'
+                name='name'
+                onChange={this.handleInput}
+                onKeyPress={this.handleBtn}
+              />
               <p className='email'>이메일</p>
-              <input className='emailBox' type='text' placeholder='이메일' />
+              <input
+                className='emailBox'
+                type='text'
+                placeholder='이메일'
+                name='email'
+                onChange={this.handleInput}
+                onKeyPress={this.handleBtn}
+              />
               <p className='password'>비밀번호</p>
               <input
                 className='passwordBox'
                 type={seePw ? 'text' : 'password'}
                 placeholder='비밀번호'
+                name='password'
+                onChange={this.handleInput}
+                onKeyPress={this.handleBtn}
               />
               {seePw ? (
                 <FaEyeSlash className='onEye' onClick={this.changeIcon} />
@@ -44,7 +89,14 @@ class SignUp extends Component {
                 <FaEye className='onEye' onClick={this.changeIcon} />
               )}
               <p className='address'>주소</p>
-              <input className='addressBox' type='text' placeholder='주소' />
+              <input
+                className='addressBox'
+                type='text'
+                placeholder='주소'
+                name='address'
+                onChange={this.handleInput}
+                onKeyPress={this.handleBtn}
+              />
             </div>
             <div className='checkBox'>
               <div className='line'></div>
@@ -58,19 +110,25 @@ class SignUp extends Component {
                   <input className='checkPoint' type='checkbox' />
                   <p className='agreedName'>이용약관 동의</p>
                   <p className='choose'>(필수)</p>
-                  <Link className='readMore'>약관보기</Link>
+                  <Link to='/signUp' className='readMore'>
+                    약관보기
+                  </Link>
                 </div>
                 <div className='subCheck'>
                   <input className='checkPoint' type='checkbox' />
                   <p className='agreedName'>개인정보 수집 및 이용 동의</p>
                   <p className='choose'>(필수)</p>
-                  <Link className='readMore'>약관보기</Link>
+                  <Link to='/signUp' className='readMore'>
+                    약관보기
+                  </Link>
                 </div>
                 <div className='subCheck'>
                   <input className='checkPoint' type='checkbox' />
                   <p className='agreedName'>마케팅 수신 동의</p>
                   <p className='choose'>(선택)</p>
-                  <Link className='readMore'>약관보기</Link>
+                  <Link to='/signUp' className='readMore'>
+                    약관보기
+                  </Link>
                 </div>
                 <div className='subCheck'>
                   <input className='checkPoint' type='checkbox' />
@@ -78,7 +136,10 @@ class SignUp extends Component {
                   <p className='choose'>(필수)</p>
                 </div>
               </div>
-              <button className='registerBox'>
+              <button
+                className='registerBox'
+                onClick={isActive ? this.goToList : this.openAlert}
+              >
                 <p className='registerStyle'>REGISTER</p>
               </button>
             </div>
