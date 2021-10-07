accf108fbcee32db788eb5870f287364a22573e6
윤창현 <changh950@gmail.com>
2021-10-07 20:35:11 +0900

    Fix: SignUp 기능 구현

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -11,82 +11,154 @@ class SignUp extends Component {
       name: '',
       email: '',
       password: '',
-      seePw: false,
+      address: '',
+      showPw: false,
+      isAllAgreeChecked: false,
+      isUseAgreeChecked: false,
+      isInformationAgreeChecked: false,
+      isMarketingAgreeChecked: false,
+      isMyselfAgreeChecked: false,
     };
   }
 
   handleInput = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value }, () => this.handleBtn());
+    this.setState({ [name]: value });
   };
 
-  handleBtn = () => {
-    const { name, email, password } = this.state;
+  clickCheckbox = event => {
+    const { name, checked } = event.target;
     this.setState({
-      isActive:
-        name.includes('/^[가-힣]+$/') &&
-        email.includes(
-          '/^([w-]+(?:.[w-]+)*)@((?:[w-]+.)*w[w-]{0,66}).([a-z]{2,6}(?:.[a-z]{2})?)$/'
-        ) &&
-        password.length >= 5,
+      [name]: checked,
     });
+
+    if (name === 'isAllAgreeChecked') {
+      if (checked) {
+        this.setState({
+          isAllAgreeChecked: true,
+          isUseAgreeChecked: true,
+          isInformationAgreeChecked: true,
+          isMarketingAgreeChecked: true,
+          isMyselfAgreeChecked: true,
+        });
+      } else {
+        this.setState({
+          isAllAgreeChecked: false,
+          isUseAgreeChecked: false,
+          isInformationAgreeChecked: false,
+          isMarketingAgreeChecked: false,
+          isMyselfAgreeChecked: false,
+        });
+      }
+    }
+
+    if (
+      name === 'isUseAgreeChecked' ||
+      name === 'isInformationAgreeChecked' ||
+      name === 'isMarketingAgreeChecked' ||
+      name === 'isMyselfAgreeChecked'
+    ) {
+      if (checked) {
+        this.setState({
+          isAllAgreeChecked: false,
+        });
+      } else {
+        this.setState({
+          isAllAgreeChecked: false,
+        });
+      }
+    }
   };
 
   changeIcon = () => {
     this.setState({
-      seePw: !this.state.seePw,
+      showPw: !this.state.showPw,
     });
   };
 
-  openAlert = () => {
+  signUpFailAlert = () => {
     alert('유효한 형식으로 작성해주세요.');
   };
 
   goToList = () => {
-    if (this.state.isActive) {
-      this.props.history.push('./SignIn');
-    }
+    this.props.history.push('./SignIn');
   };
 
   render() {
-    const { seePw, isActive } = this.state;
+    const {
+      showPw,
+      name,
+      email,
+      password,
+      address,
+      isAllAgreeChecked,
+      isUseAgreeChecked,
+      isInformationAgreeChecked,
+      isMarketingAgreeChecked,
+      isMyselfAgreeChecked,
+    } = this.state;
+
+    const inputComplete =
+      email.includes('@') &&
+      name.includes('') &&
+      password.length >= 5 &&
+      address.includes('') &&
+      isUseAgreeChecked &&
+      isInformationAgreeChecked &&
+      isMyselfAgreeChecked;
+
+    const correctName = /^[가-힣]{2,4}$/;
+    const correctEmail =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    const correctPassword =
+      /^.*(?=^.{5,12}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+
+    const checkName = name.match(correctName) || name === '';
+    const checkEmail = email.match(correctEmail) || email === '';
+    const checkPassword = password.match(correctPassword) || password === '';
+
     return (
       <section className='SignUp'>
         <form action='' className='form'>
           <div className='signUpBox'>
             <div className='inputBox'>
               <p className='name'>이름</p>
+              <p className={checkName ? 'wrongHide' : 'wrongShow'}>
+                유효한 형식으로 작성해주세요.
+              </p>
               <input
                 className='nameBox'
                 type='text'
                 placeholder='이름'
                 name='name'
                 onChange={this.handleInput}
-                onKeyPress={this.handleBtn}
               />
               <p className='email'>이메일</p>
+              <p className={checkEmail ? 'wrongHide' : 'wrongShow'}>
+                유효한 이메일 형식으로 작성해주세요.
+              </p>
               <input
                 className='emailBox'
                 type='text'
                 placeholder='이메일'
                 name='email'
                 onChange={this.handleInput}
-                onKeyPress={this.handleBtn}
               />
               <p className='password'>비밀번호</p>
+              <p className={checkPassword ? 'wrongHide' : 'wrongShow'}>
+                비밀번호는 5자리 이상, 12자리 이하여야 합니다. (문자, 특수문자
+                포함)
+              </p>
               <input
                 className='passwordBox'
-                type={seePw ? 'text' : 'password'}
+                type={showPw ? 'text' : 'password'}
                 placeholder='비밀번호'
                 name='password'
                 onChange={this.handleInput}
-                onKeyPress={this.handleBtn}
               />
-              {seePw ? (
-                <FaEyeSlash className='onEye' onClick={this.changeIcon} />
-              ) : (
-                <FaEye className='onEye' onClick={this.changeIcon} />
-              )}
+              <div className='onEye' onClick={this.changeIcon}>
+                {showPw ? <FaEyeSlash /> : <FaEye />}
+              </div>
               <p className='address'>주소</p>
               <input
                 className='addressBox'
@@ -94,7 +166,6 @@ class SignUp extends Component {
                 placeholder='주소'
                 name='address'
                 onChange={this.handleInput}
-                onKeyPress={this.handleBtn}
               />
             </div>
             <div className='checkBox'>
@@ -102,11 +173,23 @@ class SignUp extends Component {
               <p className='agreement'>이용악관 동의</p>
               <div className='checkInput'>
                 <div className='allAgree'>
-                  <input className='headPoint' type='checkbox' />
+                  <input
+                    className='headPoint'
+                    type='checkbox'
+                    name='isAllAgreeChecked'
+                    checked={isAllAgreeChecked}
+                    onClick={this.clickCheckbox}
+                  />
                   <p className='agreedName'>전체 동의 합니다.</p>
                 </div>
                 <div className='subCheck'>
-                  <input className='checkPoint' type='checkbox' />
+                  <input
+                    className='checkPoint'
+                    type='checkbox'
+                    name='isUseAgreeChecked'
+                    checked={isUseAgreeChecked}
+                    onClick={this.clickCheckbox}
+                  />
                   <p className='agreedName'>이용약관 동의</p>
                   <p className='choose'>(필수)</p>
                   <Link to='/signUp' className='readMore'>
@@ -114,7 +197,13 @@ class SignUp extends Component {
                   </Link>
                 </div>
                 <div className='subCheck'>
-                  <input className='checkPoint' type='checkbox' />
+                  <input
+                    className='checkPoint'
+                    type='checkbox'
+                    name='isInformationAgreeChecked'
+                    checked={isInformationAgreeChecked}
+                    onClick={this.clickCheckbox}
+                  />
                   <p className='agreedName'>개인정보 수집 및 이용 동의</p>
                   <p className='choose'>(필수)</p>
                   <Link to='/signUp' className='readMore'>
@@ -122,7 +211,13 @@ class SignUp extends Component {
                   </Link>
                 </div>
                 <div className='subCheck'>
-                  <input className='checkPoint' type='checkbox' />
+                  <input
+                    className='checkPoint'
+                    type='checkbox'
+                    name='isMarketingAgreeChecked'
+                    checked={isMarketingAgreeChecked}
+                    onClick={this.clickCheckbox}
+                  />
                   <p className='agreedName'>마케팅 수신 동의</p>
                   <p className='choose'>(선택)</p>
                   <Link to='/signUp' className='readMore'>
@@ -130,14 +225,20 @@ class SignUp extends Component {
                   </Link>
                 </div>
                 <div className='subCheck'>
-                  <input className='checkPoint' type='checkbox' />
+                  <input
+                    className='checkPoint'
+                    type='checkbox'
+                    name='isMyselfAgreeChecked'
+                    checked={isMyselfAgreeChecked}
+                    onClick={this.clickCheckbox}
+                  />
                   <p className='agreedName'>본인은 만 14세 이상입니다.</p>
                   <p className='choose'>(필수)</p>
                 </div>
               </div>
               <button
                 className='registerBox'
-                onClick={isActive ? this.goToList : this.openAlert}
+                onClick={inputComplete ? this.goToList : this.signUpFailAlert}
               >
                 <p className='registerStyle'>REGISTER</p>
               </button>
